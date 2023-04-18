a34311831df6430c7876ce33d112152986a458d8
Eliyahu Masinter <eliyahumasinter@gmail.com>
2023-04-19 02:30:12 +0300

    Triangle Boundary Value Tests

--- unittests/geometries/TriangleTests.java
+++ unittests/geometries/TriangleTests.java
@@ -2,6 +2,7 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +34,33 @@ class TriangleTests {
             assertTrue(isZero(result.dotProduct(pts[i].subtract(pts[i == 0 ? 3 : i - 1]))),
                     "Polygon's normal is not orthogonal to one of the edges");
     }
+
+    @Test
+    void testFindIntersections() {
+        Point[] pts =
+                { new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0), new Point(1, 1, 0) };
+        Triangle triangle = new Triangle(pts[0], pts[1], pts[2]);
+        Vector v = pts[0].subtract(pts[1]);
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Ray intersects triangle interior (1 point)
+
+        // TC02: Ray intersects line extensions at vertex
+
+        // TC03: Ray intersects line extensions at base
+
+        // =============== Boundary Values Tests ==================
+        // TC11: Ray intersects triangle side extension  (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(-1, 0, -1), new Vector(0, 0, 1))), "Ray intersects side extension");
+
+        // TC12: Ray intersects triangle side (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(0.5, 0, -1), new Vector(0, 0, 1))), "Ray intersects side");
+
+        // TC13: Ray intersects triangle vertex (0 points)
+        assertNull(triangle.findIntersections(new Ray(new Point(0, 0, -1), new Vector(0, 0, 1))), "Ray intersects triangle vertext");
+
+    }
+
 
 
 }
