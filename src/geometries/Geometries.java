3a3ae057b1cde0ba27d6e98da204447e50fcbc8e
Eliyahu Masinter <eliyahumasinter@gmail.com>
2023-04-20 06:36:49 +0300

    Geometries Collection Test and Implementation

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,36 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.List;
+import java.util.LinkedList;
+public class Geometries implements Intersectable{
+    private List<Intersectable> allGeometries;
+    public Geometries(){
+        allGeometries = new LinkedList<Intersectable>();
+    }
+
+    public void add(Intersectable... geometries){
+        for( Intersectable is : geometries ) {
+            allGeometries.add(is);
+        }
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray){
+        List<Point> points = new LinkedList<Point>();
+        for ( Intersectable g : allGeometries){
+            List<Point> geometryPoints = g.findIntersections(ray);
+            if (g != null){
+                for (Point p : geometryPoints){
+                    points.add(p);
+                }
+            }
+        }
+        if (points.size() > 0)
+            return points;
+        return null;
+    }
+
+}
---
+++ unittests/geometries/GeometriesTests.java
@@ -0,0 +1,47 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeometriesTests {
+
+    @Test
+    void findIntersections() {
+
+        // ============ Equivalence Partitions Tests ==============
+
+        //TC01: Several (but not all) geometries intersect (
+        Geometries geometries = new Geometries();
+        geometries.add(new Plane(new Point(0,0,1),new Point(1,0,1),new Point(0,1,1)))
+        geometries.add(new Sphere(new Point(10,0,1), 1));
+        Ray ray = new Ray(new Point(0,0,-1), new Vector(0,0,1));
+        assertEquals(1, geometries.findIntersections(ray).size(), "all but 1 intersect");
+
+        // ======= Boundary Value Analysis Tests ==============
+
+        //TC11: Empty Geometries Collection
+        geometries = new Geometries();
+        ray = new Ray(new Point(-1,0,0), new Vector(1,0,0));
+        assertNull(geometries.findIntersections(ray), "Empty Geometry Collection");
+
+        //TC 12: No Intersection Point
+        geometries.add(new Sphere(new Point(1,0,10), 1));
+        assertNull(geometries.findIntersections(ray), "No intersections");
+
+        //TC 13: Only one geometry intersects
+        geometries.add(new Sphere(new Point(1,0,0), 1));
+        assertEquals(1, geometries.findIntersections(ray).size(), "1 intersection");
+
+        //TC 14: All geometries intersect
+        geometries = new Geometries();
+        geometries.add(new Plane(new Point(0,0,1),new Point(1,0,1),new Point(0,1,1)))
+        geometries.add(new Sphere(new Point(0,0,5), 1));
+        ray = new Ray(new Point(0,0,-1), new Vector(0,0,1));
+        assertEquals(3, geometries.findIntersections(ray).size(), "all intersect");
+
+    }
+}
