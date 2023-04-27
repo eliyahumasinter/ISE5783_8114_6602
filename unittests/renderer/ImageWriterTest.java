ff7115e49bcb9c50a2cb54906d05366b343559b8
Eliyahu Masinter <eliyahumasinter@gmail.com>
2023-04-27 16:10:18 +0300

    Image Writer

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -1,2 +1,24 @@
-package renderer;public class ImageWriterTest {
+package renderer;
+
+import org.junit.jupiter.api.Test;
+import java.awt.Color;
+import renderer.ImageWriter;
+public class ImageWriterTest {
+
+    /**
+     * Test method for {@link renderer.ImageWriter#ImageWriter(java.lang.String, int, int)}.
+     */
+    @Test
+    public void testImageWriter() {
+        var writer = new ImageWriter("image", 800, 500);
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 800; j++) {
+                if (i % 50 == 0 || j % 50 == 0 || i == 799 || j == 499)
+                    writer.writePixel(j, i, new primitives.Color(Color.red));
+                else
+                    writer.writePixel(j, i, new primitives.Color(Color.yellow));
+            }
+        }
+        writer.writeToImage();
+    }
 }
